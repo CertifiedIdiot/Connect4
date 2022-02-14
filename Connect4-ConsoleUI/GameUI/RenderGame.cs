40b6509edfe8d213bb58f4c5e73f3b092c8aff2d
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-02-14 23:44:40 +0100

    RenderGameElement WIP

--- Connect4-ConsoleUI/GameUI/RenderGame.cs
+++ Connect4-ConsoleUI/GameUI/RenderGame.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Connect4_ConsoleUI.UIProperties;
+using Console = Colorful.Console;
 
 namespace Connect4_ConsoleUI.GameUI
 {
-    public class RenderGame
+    internal static class RenderGame
     {
 
+        // Start => Display stuff
+
+
+        internal static char[,] GetCharBoardArrayTest() => new char[,]
+    {
+                { '*','*','*','*','*','*','*'},
+                { '*','o','*','*','*','*','*'},
+                { '*','x','*','*','*','*','*'},
+                { '*','x','o','*','*','*','*'},
+                { 'o','x','x','o','*','*','*'},
+                { 'x','o','o','x','*','*','*'},
+     };
+
+        /// <summary>
+        /// Sets the console settings. Adjusts the console height to fit the UI, and changes the console background colour.
+        /// </summary>
+        internal static void SetConsoleSettings()
+        {
+            Console.WindowHeight += 10;
+            Console.BackgroundColor = UIColours.BackgroundColour;
+            Console.Clear();
+        }
     }
 }
--- Connect4-ConsoleUI/GameUI/RenderGameElement.cs
+++ Connect4-ConsoleUI/GameUI/RenderGameElement.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Connect4_ConsoleUI.Resources;
+using Connect4_ConsoleUI.UIHelpers;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,14 @@ namespace Connect4_ConsoleUI.GameUI
 {
     public class RenderGameElement
     {
+        public static void BackgroundTable(int posX, int posY, Color tableColour)
+        {
+            Print.AtPosition(ASCIIGraphics.tableArray, posX, posY, tableColour);
+        }
 
+        public static void GameBoard(int posX, int posY, Color gameboardColour)
+        {
+            Print.AtPosition(ASCIIGraphics.gameboardArray, posX, posY, gameboardColour);
+        }
     }
 }

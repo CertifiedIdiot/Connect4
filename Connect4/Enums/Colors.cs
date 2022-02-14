909074d89954815efef288f91207d4347e711108
Thomas Thorin <thomas.thorin@gmail.com>
2022-02-14 14:06:21 +0100

    added struct for slots and enum for colors

---
+++ Connect4/Enums/Colors.cs
@@ -0,0 +1,15 @@
+﻿namespace Connect4.Enums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public enum Colors
+    {
+        None,
+        Red,
+        Yellow
+    }
+}
---
+++ Connect4/Structs/Slot.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------------------------------
+//  Class1.cs by Thomas Thorin, Copyright (C) 2022.
+//  Published under GNU General Public License v3 (GPL-3)
+// -----------------------------------------------------------------------------------------------
+
+namespace Connect4.Structs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Class1
+    {
+    }
+}

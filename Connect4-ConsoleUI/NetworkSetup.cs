5a38039ed7e4f3f67ceb62c45bee7ba48b803c65
Thomas Thorin <thomas.thorin@gmail.com>
2022-02-16 21:39:13 +0100

    some refactoring and network related changes

--- Connect4-ConsoleUI/NetworkSetup.cs
+++ Connect4-ConsoleUI/NetworkSetup.cs
@@ -1,10 +1,7 @@
 ﻿namespace Connect4_ConsoleUI
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.Text.RegularExpressions;
 
     internal class NetworkSetup
     {
@@ -17,8 +14,8 @@
             var input = Console.ReadKey(true);
             switch (input.Key)
             {
-                case ConsoleKey.D1 or ConsoleKey.NumPad1: StartServer(); break;
-                case ConsoleKey.D2 or ConsoleKey.NumPad2: StartClient();break;
+                case ConsoleKey.D1 or ConsoleKey.NumPad1: StartNetwork(true); break;
+                case ConsoleKey.D2 or ConsoleKey.NumPad2: StartNetwork(false); break;
                 case ConsoleKey.D3 or ConsoleKey.NumPad3: StartHotSeat(); break;
                 case ConsoleKey.Escape: Environment.Exit(0); break;
                 default:
@@ -26,26 +23,41 @@
             }
         }
 
-        private void StartHotSeat() => new QuickTest(null,true).Run();
-        private void StartServer()
+        private void StartHotSeat() => new QuickTest(null!, true).Run();
+        private void StartNetwork(bool startAsServer)
         {
-            var server = Connect4.Connect4Factory.GetServer();
-            Console.Write("Enter IP you want to host on: ");
-            server.IP = Console.ReadLine();
+            var network = startAsServer ? Connect4.Connect4Factory.GetServer() : Connect4.Connect4Factory.GetClient();
+            Console.Write(startAsServer ? "Enter IP you want to host on: " : "Enter IP you want to connect on: ");
+            network.IP = AskForIP();
             Console.WriteLine("Waiting for connection...");
-            Console.WriteLine(server.Start());
+            Console.WriteLine(network.Start());
             Console.ReadLine();
-            new QuickTest(server,true).Run();
+            if (startAsServer) new QuickTest(network, true).Run();
+            else new QuickTest(network, false).Run();
         }
-        private void StartClient()
+
+        private string AskForIP()
         {
-            var client = Connect4.Connect4Factory.GetClient();
-            Console.Write("Enter IP you want to connect on: ");
-            client.IP = Console.ReadLine();
-            Console.WriteLine("Waiting for connection...");
-            Console.WriteLine(client.Start());
-            Console.ReadLine();
-            new QuickTest(client,false).Run();
+            var input = Console.ReadLine();
+            while (!ValidIP(input!))
+            {
+                Console.Write("Please enter a valid IP: ");
+                input = Console.ReadLine();
+            }
+            return input!;
+        }
+
+        private bool ValidIP(string ip)
+        {
+            var regex = new Regex(@"^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$");
+            if (!regex.IsMatch(ip)) return false;
+            var numbers = ip.Split('.');
+            foreach (var number in numbers)
+            {
+                _ = int.TryParse(number, out int num);
+                if (num < 0 || num > 255) return false;
+            }
+            return true;
         }
     }
 }

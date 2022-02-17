a2c7d72178e5c7e294e74e97c154fa28759cb9cb
Alaa Alkhalifa <rose.style111@gmail.com>
2022-02-17 18:46:38 +0100

    Greate class unit for JsonHandler

---
+++ Connect4.UnitTest/JsonHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4.UnitTest
+{
+    [TestFixture]
+    public class JsonHandlerTests
+    {
+        
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void Serializ_DataObject_ReturnJsonFormat()
+        {
+            var json = JsonHandler.Serialize("ABC");
+            Assert.That(json, Is.Not.Null);
+            Assert.That(json, Does.Contain("ABC"));
+        }
+
+        [Test]
+        public void Serializ_DataObjectIsNull_ReturnNoThing()
+        {
+            var json = JsonHandler.Serialize("");
+            
+            Assert.That(() => json, Throws.Nothing);
+        }
+
+        [Test]
+        public void Deserialize_JsonFormat_ReturnDataOpject()
+        {
+
+            //string input = "ABC.json";
+            //object t = JsonHandler.Deserialize(input).ToString();
+            //object dataOpject = new object();
+            //string StrJson = "ABC";
+            //var dataOpject = JsonConvert.DeserializeObject<dynamic>(StrJson);
+            //Assert.That(dataOpject, Is.EqualTo(JsonHandler.Deserialize<dynamic>(StrJson)));
+
+            object dataOpject = new object();
+            string strJson = "ABC";
+            
+            dataOpject = JsonHandler.Deserialize<dynamic>(strJson);
+            
+            Assert.That(dataOpject, 
+
+
+
+        }
+    }
+}
---
+++ Connect4.UnitTests/Connect4Tests/JsonHandlerTests.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4.UnitTests.Connect4Tests
+{
+    [TestFixture]
+    public class JsonHandlerTests
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Serializ_DataObject_ReturnJsonFormat()
+        {
+            var json = JsonHandler.Serialize("ABC");
+            Assert.That(json, Is.Not.Null);
+            Assert.That(json, Does.Contain("ABC"));
+        }
+
+        [Test]
+        public void Serializ_DataObjectIsNull_ReturnNoThing()
+        {
+            var json = JsonHandler.Serialize("");
+
+            Assert.That(() => json, Throws.Nothing);
+        }
+
+        [Test]
+        public void Deserialize_JsonFormat_ReturnDataOpject()
+        {
+
+            string json = @"{'Name': 'C-sharpcorner', 'Description': 'Share Knowledge' }";
+            var result = JsonHandler.Deserialize<object>(json).ToString();
+            
+            Assert.That(result, Does.StartWith("{"));
+            Assert.That(result, Does.Contain("Name"));
+            Assert.That(result, Does.Contain("C-sharpcorner"));
+            Assert.That(result, Does.Contain("Description"));
+            Assert.That(result, Does.Contain("Share Knowledge"));
+            Assert.That(result, Does.EndWith("}"));
+
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Deserialize_JsonFileIsEmpty_ReturnNoData(string json)
+        {
+
+            //string json = "";
+            var result = JsonHandler.Deserialize<object>(json);
+
+            Assert.That(() => result, Throws.Nothing);
+           
+        }
+    }
+}

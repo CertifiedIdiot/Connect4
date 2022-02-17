f6664c0ac868106483fde42166bf3fffb0f344b3
Thomas Thorin <thomas.thorin@gmail.com>
2022-02-17 13:51:00 +0100

    cleanup

--- Connect4-ConsoleUI/NetworkSetup.cs
+++ Connect4-ConsoleUI/NetworkSetup.cs
@@ -6,15 +6,10 @@
     internal class NetworkSetup
     {
         public void Run()
-        {
-            //Console.WriteLine("1) Start as server.");
-            //Console.WriteLine("2) Start as client.");
-            //Console.WriteLine("3) Start a hot seat game.");
-            //Console.WriteLine("Pick a number or ESC to exit.");
-            //var input = Console.ReadKey(true);
+        { 
             var menuItems = new List<string>() { "Network setup", "Start as server.", "Start as client", "Start hot seat game.", "Exit." };
-
             var input = new Menu(menuItems).UseMenu();
+
             switch (input)
             {
                 case "Start as server.": StartNetwork(true); break;
@@ -24,15 +19,6 @@
                 default:
                     break;
             }
-            //switch (input.Key)
-            //{
-            //    case ConsoleKey.D1 or ConsoleKey.NumPad1: StartNetwork(true); break;
-            //    case ConsoleKey.D2 or ConsoleKey.NumPad2: StartNetwork(false); break;
-            //    case ConsoleKey.D3 or ConsoleKey.NumPad3: StartHotSeat(); break;
-            //    case ConsoleKey.Escape: Environment.Exit(0); break;
-            //    default:
-            //        break;
-            //}
         }
 
         private void StartHotSeat() => new QuickTest(null!, true).Run();

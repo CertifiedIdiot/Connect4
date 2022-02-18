4697eb2cdd6d41d9c7f0295525fdf8a6542de7af
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-02-18 21:14:45 +0100

    Meh solution to manually add and change colourschemes

--- Connect4-ConsoleUI/UIProperties/UIColours.cs
+++ Connect4-ConsoleUI/UIProperties/UIColours.cs
@@ -10,12 +10,38 @@ namespace Connect4_ConsoleUI.UIProperties
     public static class UIColours
     {
         // TODO: Feel free to try out other colours to create a nice looking colour scheme
-        // TODO: Colourscheme to make it easier to switch all colours at once?
-        internal static Color PlayerOneColour { get; set; } = Color.DodgerBlue;     // Original colour - Color.DodgerBlue
-        internal static Color PlayerTwoColour { get; set; } = Color.OrangeRed;     // Original colour - Color.OrangeRed
-        internal static Color GameboardColour { get; set; } = Color.CadetBlue;     // Original colour - Color.CadetBlue
-        internal static Color TableColour { get; set; } = Color.SaddleBrown;      // Original colour - Color.SaddleBrown
-        internal static Color BackgroundColour { get; set; } = Color.Bisque;     // Original colour - Color.Bisque
-        internal static Color TextColour { get; set; } = Color.DarkCyan;
+
+        /// <summary>
+        /// <para> List containing different colour schemes for the game. Each colour scheme should have 6 colours. </para>
+        ///
+        /// <para> To manually change scheme used, edit the first number in the property, eg: </para>
+        ///
+        /// <para> Color.FromName(ColourSchemes[0][0]) => Color.FromName(ColourSchemes[1][0]) => the PlayerOneColour has changed to the second colour scheme in the ColourSchemes list. </para>
+        /// </summary>
+        private static readonly List<List<string>> ColourSchemes = new List<List<string>>()
+        {
+        // Colour order - PlayerOneColour, PlayerTwoColour, GameboardColour, TableColour, BackgroundColour, Textcolour
+            new List<string> { "DodgerBlue", "OrangeRed", "CadetBlue", "SaddleBrown", "Bisque", "DarkCyan"},
+            new List<string> { ""},
+            //new List<string> {"", "", "", "", "", "" },
+        };
+
+        /// <summary> Gets or sets the player one colour See also <seealso cref="ColourSchemes"/> for more info. </summary>
+        internal static Color PlayerOneColour { get; set; } = Color.FromName(ColourSchemes[0][0]);   // Original colour - Color.DodgerBlue
+
+        /// <summary> Gets or sets the player two colour colour. See also <seealso cref="ColourSchemes"/> for more info. </summary>
+        internal static Color PlayerTwoColour { get; set; } = Color.FromName(ColourSchemes[0][1]);     // Original colour - Color.OrangeRed
+
+        /// <summary> Gets or sets the gameboard colour. See also <seealso cref="ColourSchemes"/> for more info. </summary>
+        internal static Color GameboardColour { get; set; } = Color.FromName(ColourSchemes[0][2]);      // Original colour - Color.CadetBlue
+
+        /// <summary> Gets or sets the table element colour. See also <seealso cref="ColourSchemes"/> for more info. </summary>
+        internal static Color TableColour { get; set; } = Color.FromName(ColourSchemes[0][3]);      // Original colour - Color.SaddleBrown
+
+        /// <summary> Gets or sets the background colour. See also <seealso cref="ColourSchemes"/> for more info. </summary>
+        internal static Color BackgroundColour { get; set; } = Color.FromName(ColourSchemes[0][4]);      // Original colour - Color.Bisque                
+
+        /// <summary> Gets or sets the text colour. See also <seealso cref="ColourSchemes"/> for more info. </summary>
+        internal static Color TextColour { get; set; } = Color.FromName(ColourSchemes[0][5]);           // Original colour - Color.DarkCyan
     }
 }

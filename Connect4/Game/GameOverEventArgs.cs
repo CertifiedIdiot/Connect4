40f322946aa366bdb06597f3ae523979fce4810a
Thomas Thorin <thomas.thorin@gmail.com>
2022-02-19 18:35:01 +0100

    changed events to use EventArgs for easier unit testing

---
+++ Connect4/Game/GameOverEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace Connect4.Game
+{
+    using Connect4.Enums;
+
+    public class GameOverEventArgs:EventArgs
+    {
+        public string Winner { get; }
+        public GameOverEventArgs(string winner)
+        {
+            Winner = winner;
+        }
+    }
+}

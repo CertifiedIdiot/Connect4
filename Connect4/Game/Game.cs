5f99e0a370a2df50c4a4a1a0fa7470a1c35fdf7f
Thomas Thorin <thomas.thorin@gmail.com>
2022-02-21 22:48:21 +0100

    all the xml

--- Connect4/Game/Game.cs
+++ Connect4/Game/Game.cs
@@ -86,7 +86,7 @@ namespace Connect4.Game
         /// </summary>
         ~Game()
         {
-            if (network != null) network.Stop();
+            network?.Stop();
         }
 
         /// <summary>
@@ -111,6 +111,11 @@ namespace Connect4.Game
             if (network != null! && ActivePlayer.PlayerNumber != InstanceId) ReceiveGameState();
         }
 
+        /// <summary>
+        /// Attempts to place active players token in the given column. Will raise a <see cref="BoardChangedEvent"/> on a succesful move and a <see cref="GameOverEvent"/> on a winning or draw (full board) move.
+        /// </summary>
+        /// <param name="column">The column.</param>
+        /// <returns><see langword="true"/> if the token is placed succesfully and <see langword="false"/> if for some reason the move failed (like invalid or full column).</returns>
         public bool MakeMove(int column)
         {
             if (ValidMove(column, out int row))
@@ -124,6 +129,12 @@ namespace Connect4.Game
             return false;
         }
 
+        /// <summary>
+        /// Method for checking that an attempted move is made in a valid, non-full column.
+        /// </summary>
+        /// <param name="column">The column the move is attempted in.</param>
+        /// <param name="row">The out int parameter row "returned" for the correct row to place a token in on a valid move.</param>
+        /// <returns><see langword="true"/> if the move is determined to be valid, <see langword="fasle"/> if not.</returns>
         private bool ValidMove(int column, out int row)
         {
             row = -1;
@@ -135,6 +146,9 @@ namespace Connect4.Game
             return false;
         }
 
+        /// <summary>
+        /// Updates the state of the game after a valid move, and, if in a network game, calls <see cref="SendGameState"/> to send the game state to the opponent.
+        /// </summary>
         private void UpdateGameState()
         {
             MoveCounter++;
@@ -143,12 +157,21 @@ namespace Connect4.Game
             if (network != null!) SendGameState();
         }
 
+        /// <summary>
+        /// The method that actually places the correct <see cref="Token"/> on the <see cref="Board"/> in the given position.
+        /// </summary>
+        /// <param name="column">The column of the <see cref="Board"/> where the <see cref="Token"/> should be placed.</param>
+        /// <param name="row">The row of the <see cref="Board"/> where the <see cref="Token"/> should be placed.</param>
         private void PlaceToken(int column, int row)
         {
             Board[column, row].State = ActivePlayer.PlayerNumber;
             BoardChangedEvent?.Invoke(this, EventArgs.Empty);
         }
 
+        /// <summary>
+        /// In a network game, will send collect and send the current game state to the opponent, and unless it's a draw game (42 moves made),
+        /// call <see cref="ReceiveGameState"/> to be ready to receive the game state after the opponents move.
+        /// </summary>
         private void SendGameState()
         {
             var json = JsonHandler.Serialize(new GameState()
@@ -162,6 +185,9 @@ namespace Connect4.Game
             if (gameWonBy == Token.None && MoveCounter != 43) ReceiveGameState();
         }
 
+        /// <summary>
+        /// Receives the state of the game during a network game and updates this <see cref="Game"/> instance with the <see cref="GameState"/> information recieved. Will raise <see cref="BoardChangedEvent"/> when updating instance and will also raise a <see cref="GameOverEvent"/> on receit of a win or draw.
+        /// </summary>
         private void ReceiveGameState()
         {
             var json = network.Receive();
@@ -175,11 +201,21 @@ namespace Connect4.Game
             else if (MoveCounter == 43) GameOverEvent?.Invoke(this, new GameOverEventArgs("Draw."));
         }
 
+        /// <summary>
+        /// Calls <see cref="CheckDirection(int, int)"/> to search the <see cref="Board"/> for four in a row after a valid move.
+        /// </summary>
+        /// <returns><see langword="true"/> if four in a row is found, <see langword="true"/> if not.</returns>
         private bool CheckForFour() => CheckDirection(1, 0)
             || CheckDirection(1, 1)
             || CheckDirection(0, 1)
             || CheckDirection(-1, 1);
 
+        /// <summary>
+        /// Iterates through all the <see cref="Slot"/>s of the <see cref="Board"/> to try and find four in a row. Will raise a <see cref="GameOverEvent"/> if four in a row is found.
+        /// </summary>
+        /// <param name="deltaColumn">In what column in relation to the current to check next for connections, -1 for left, 0 for same, 1 for right.</param>
+        /// <param name="deltaRow">In what row in relation to the current to check next for connections, -1 for up, 0 for same, 1 for down.</param>
+        /// <returns><see langword="true"/> if four in a row is found, <see langword="false"/> if not.</returns>
         private bool CheckDirection(int deltaColumn, int deltaRow)
         {
             for (int row = 0; row <= Board.GetUpperBound(1); row++)
@@ -198,6 +234,14 @@ namespace Connect4.Game
             return false;
         }
 
+        /// <summary>
+        /// Searches adjacent <see cref="Slot"/>s in the <see cref="Board"/> to find four in a row.
+        /// </summary>
+        /// <param name="column">The column to start looking from.</param>
+        /// <param name="row">The row to start looking from.</param>
+        /// <param name="deltaColumn">In what column in relation to the current to check next for connections, -1 for left, 0 for same, 1 for right.</param>
+        /// <param name="deltaRow">In what row in relation to the current to check next for connections, -1 for up, 0 for same, 1 for down.</param>
+        /// <returns>The number of connecting <see cref="Slot"/> found that was occupied by the <see cref="ActivePlayer"/>s <see cref="Token"/>.</returns>
         private int FindConnecting(int column, int row, int deltaColumn, int deltaRow)
         {
             int columnToCheck = column, rowToCheck = row, counter = 0;
@@ -209,9 +253,28 @@ namespace Connect4.Game
             }
             return counter;
         }
+
+        /// <summary>
+        /// Determines if the currently checked <see cref="Slot"/> is occupied by the <see cref="ActivePlayer"/>s <see cref="Token"/>.
+        /// </summary>
+        /// <param name="column">The column to check.</param>
+        /// <param name="row">The row to check.</param>
+        /// <returns><see langword="true"/> if the checked slot is occupied by the active player, <see langword="false"/> if not.</returns>
         private bool SlotOwnedByActivePlayer(int column, int row) => Board[column, row].State == ActivePlayer.PlayerNumber;
+
+        /// <summary>
+        /// Determines if the given coordinates for a <see cref="Slot"/> of the board to check falls within the bounds of the <see cref="Board"/> array.
+        /// </summary>
+        /// <param name="x">The column to check.</param>
+        /// <param name="y">The the row to check.</param>
+        /// <returns><see langword="true"/> if the slot is within bounds, <see langword="false"/> if not.</returns>
         private bool ValidCell(int x, int y) => x >= 0 && x <= Board.GetUpperBound(0) && y >= 0 && y <= Board.GetUpperBound(1);
 
+        /// <summary>
+        /// Checks the column to see if it is full or has room to place a <see cref="Token"/> in it.
+        /// </summary>
+        /// <param name="column">The column to check.</param>
+        /// <returns>-1 if the column is full, otherwise the number corresponding to the "bottom most" row available.</returns>
         private int CheckColumn(int column)
         {
             for (int i = Board.GetUpperBound(1); i >= 0; i--)
